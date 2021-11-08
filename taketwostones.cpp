f8ad3c68ef6cdda2876d96c3059dcc8594d766c8
Ethan Elnathan Mathias <91774181+ethanelnathan@users.noreply.github.com>
2021-11-08 15:07:11 +0700

    Add files via upload

---
+++ taketwostones.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x;
+    scanf("%d",&x);
+    if (x % 2 == 0){
+        printf("Bob");
+    }
+    else {
+        printf("Alice");
+    }
+    return 0;
+}
